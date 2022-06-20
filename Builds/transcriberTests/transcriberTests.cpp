1c6b1eae16c2e2587644eb0e2624462f4cfd757e
thronplunder <alessandrokiefer@gmail.com>
2022-06-20 12:03:34 +0200

    add some more testing

--- Builds/transcriberTests/transcriberTests.cpp
+++ Builds/transcriberTests/transcriberTests.cpp
@@ -13,7 +13,14 @@ namespace transcriberTests
 		TEST_METHOD(TestMethod1)
 		{
 			Window<float> testwindow{ 256 };
+			float testData1[265]= {};
+
+			testwindow.applyWindow(testData1, 256, windowType::Sine);
+
 			Assert::AreEqual<int>(256, testwindow.getSize());
+			for (int i = 0; i < 256; ++i) {
+				Assert::AreEqual<float>(testData1[i], 0);
+			}
 		}
 	};
 }
