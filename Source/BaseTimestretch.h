141c3952826e66cebdd09439e08c610bdb5e2129
thronplunder <alessandrokiefer@gmail.com>
2022-07-05 19:42:12 +0200

    add more files

---
+++ Source/BaseTimestretch.h
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    BaseTimestretch.h
+    Created: 1 Jul 2022 8:19:53pm
+    Author:  aless
+
+  ==============================================================================
+*/
+
+#pragma once
+template<typename T>
+class BaseTimestretch {
+public:
+    BaseTimestretch(int length, int channels);
+};
